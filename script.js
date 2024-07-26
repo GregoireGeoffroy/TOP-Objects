a92c998490a8e65ad8d0233a067660cd9ec775b9
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-26 17:36:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let user = {     // an object
-    name: "John",  // by key "name" store value "John"
-    age: 30        // by key "age" store value 30
-  };
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+user.name = "Pete";
+delete user.name;
 
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  
+  let sum = 0;
+  for (let key in salaries) {
+    sum += salaries[key];
+  }
+  
+  alert(sum); // 390
+
+
+  
